91f57ef1cbc3c2d598bb276451ba8e64570215c4
BoonruksaBL036 <boonruksawinanon1234@gmail.com>
2025-12-18 16:35:35 +0700

    middleware

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,8 +2,9 @@ const PostModel = require("../models/Post");
 const UserModel = require("../models/User");
 
 exports.createPost = async (req, res) => {
-  const { title, summary, content, cover, authorId } = req.body;
-  if (!title || !summary || !content || !cover || !authorId) {
+  const { title, summary, content, cover } = req.body;
+  const authorId = req.authorId;
+  if (!title || !summary || !content || !cover) {
     return res.status(400).send({
       message: "Please provide all fields.",
     });
@@ -49,8 +50,9 @@ exports.createPost = async (req, res) => {
 
 exports.getAllPost = async (req, res) => {
   try {
-    const posts = (await PostModel.find().populate("author", ["username"]))
-      .toSorted({ createdAt: -1 })
+    const posts = await PostModel.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
       .limit(20);
     if (!posts) {
       return res.status(404).send({ message: "Post not found" });
@@ -62,5 +64,115 @@ exports.getAllPost = async (req, res) => {
     });
   }
 };
-exports.getById = async (req, res) => {};
-exports.getByAuthorId = async (req, res) => {};
+exports.getById = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).send({
+      message: "Post Id is missing",
+    });
+  }
+  try {
+    const post = await PostModel.findById(id)
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20);
+    if (!post) {
+      return res.status(404).send({
+        message: "Post not found",
+      });
+    }
+    res.send(post);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "Something occurred while retrieving posts",
+    });
+  }
+};
+exports.getByAuthorId = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).send({
+      message: "Author id is missing",
+    });
+  }
+  try {
+    const posts = await PostModel.find({ author: id })
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20);
+    if (!posts) {
+      return res.status(404).send({
+        message: "Post not found",
+      });
+    }
+    res.send(posts);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "Something occurred while retrieving posts",
+    });
+  }
+};
+exports.updatePost = async (req, res) => {
+  const { id } = req.params;
+  const authorId = req.authorId;
+  if (!id) {
+    return res.status(400).send({
+      message: "Post Id is missing",
+    });
+  }
+  const { title, summary, content, cover} = req.body;
+  if (!title || !summary || !content || !cover ) {
+    return res.status(400).send({
+      message: "Please provide all fields",
+    });
+  }
+  try {
+    const postDoc = await PostModel.findOne({ _id: id, author: authorId });
+    if (!postDoc) {
+      return res
+        .status(404)
+        .send({ message: "Post with this author id is not found" });
+    }
+    if (postDoc.length < 0) {
+      return res.status(403).send({
+        message:
+          "Unauthorize to edit this post, because you are not the author of this post",
+      });
+    } else {
+      postDoc.title = title;
+      postDoc.summary = summary;
+      postDoc.content = content;
+      postDoc.cover = cover;
+      await postDoc.save();
+      res.send({ message: "Post update successfully" });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "Something occurred while retrieving posts",
+    });
+  }
+};
+exports.deletePost = async (req, res) => {
+  const { id } = req.params;
+  const author = req.authorId;
+  if (!id) {
+    return res.status(400).send({
+      message: "Post Id is missing",
+    });
+  }
+  if (!author) {
+    return res.status(400).send({
+      message: "Author Id is missing",
+    });
+  }try {
+    const postDoc = await PostModel.findByIdAndDelete({ author, _id: id });
+    if (!postDoc) {
+      return res.status(500).send({ message: "Cannot delete this post" });
+    }
+    res.send({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "Something occurred while retrieving posts",
+    });
+  }
+};
--- routers/post.router.js
+++ routers/post.router.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const PostController = require("../controllers/post.controller");
+const authJwt = require("../middlewares/authJwt.middleware");
 
 //http://localhost:5000/api/v1/post
-router.post("", PostController.createPost);
+router.post("",authJwt.verifyToken, PostController.createPost);
 //http://localhost:5000/api/v1/post
 router.get("", PostController.getAllPost);
 //http://localhost:5000/api/v1/post/id
 router.get("/:id", PostController.getById);
 //http://localhost:5000/api/v1/post/author/id
-router.get("author/:id", PostController.getByAuthorId);
+router.get("/author/:id", PostController.getByAuthorId);
 //http://localhost:5000/api/v1/post/id
-router.put("/:id", PostController.updatePost);
+router.put("/:id", authJwt.verifyToken, PostController.updatePost);
 //http://localhost:5000/api/v1/post/id
-router.delete("/:id", PostController.deletePost);
+router.delete("/:id", authJwt.verifyToken, PostController.deletePost);
 module.exports = router;
